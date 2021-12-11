d54916ea27863409978591696bd4ec2e3d196df9
arahansa <arahansa@naver.com>
2021-12-11 15:37:55 +0900

    add index 3

--- index3.js
+++ index3.js
@@ -3,6 +3,8 @@
  생성일 : 2021-11-27
  */
 import ws from "ws";
+import readline from 'readline';
+
 Object.assign(global, { WebSocket: ws });
 import SockJS from 'sockjs-client'
 
@@ -12,6 +14,32 @@ const webSockUrl = 'http://localhost:8090/webSocket';
 const sockJS = new SockJS(webSockUrl)
 console.log('sockJs :', sockJS);
 const stompClient = Stomp.over(sockJS)
+console.log('stompClient :', stompClient);
 stompClient.connect({}, () => {
-    console.log('스톰 클라이언트 최초 커넥트..? connected will be true')
+    console.log('스톰 클라이언트 최초 커넥트..? connected will be true');
+
+    stompClient.subscribe('/topic/ko', (data) => {
+        console.log('subscribe message :', data.body);
+    })
 })
+/*
+stompClient.subscribe('/private/user', (data) => {
+    console.log('subscribe message :', data.body);
+})*/
+
+
+function askQuestion(query) {
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
+
+    return new Promise(resolve => rl.question(query, ans => {
+        rl.close();
+        resolve(ans);
+    }))
+}
+
+
+const ans = await askQuestion("Are you sure you want to deploy to PRODUCTION? ");
+
